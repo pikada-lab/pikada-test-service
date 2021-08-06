487b50d0a09fe3a107798e346fe3b13e90c9ee8d
Anton A Dzhigurda <info@fake-mm.ru>
2021-08-06 17:38:14 +0300

    feature.Naming. add last name

--- Naming.js
+++ Naming.js
@@ -1,15 +1,22 @@
 module.exports.getName = () => {
-    const names = ['Антон', 'Алексей', 'Артём', 'Илья', 'Вадим', "Владимир", 
-    "Виктор", "Григорий", "Даниил", "Александр", "Павел", "Кирилл"];
+    const names = ['Антон', 'Алексей', 'Артём', 'Илья', 'Вадим', "Владимир",
+        "Виктор", "Григорий", "Даниил", "Александр", "Павел", "Кирилл"];
     return names[Math.floor(Math.random() * (names.length))];
 };
 
 module.exports.getSecondName = () => {
     const names = ["Алексеевич", "Антонович", "Артёмович", "Вадимович", "Ильич",
-    "Владимирович", "Даниилович","Георгиевич","Александрович", "Павлович","Кирилович"];
+        "Владимирович", "Даниилович", "Георгиевич", "Александрович", "Павлович", "Кирилович"];
     return names[Math.floor(Math.random() * (names.length))];
 };
 
 module.exports.getFullName = () => {
     return this.getName() + " " + this.getSecondName();
 };
+
+
+module.exports.getLastName = () => {
+    const names = ["Иванов", "Петров", "Сидоров", "Лебедев", "Попов",
+        "Коршунов", "Смирнов", "Кузнецов", "Соколов", "Новиков", "Волков"];
+    return names[Math.floor(Math.random() * (names.length))];
+}
